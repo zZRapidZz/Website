9e6a823047e517bf9284eac56272922a536fe238
zZRapidZz <43920234+zZRapidZz@users.noreply.github.com>
2023-06-08 00:53:39 +1200

    Update Assemble.js

--- generators/Assemble.js
+++ generators/Assemble.js
@@ -234,7 +234,7 @@ const GenerateBodyV2 = async function (
                         <div class="Flexbox Slide2">
                             <div class="GridItem Slide2">
                                 <h3>Web Development</h3>
-                                <p><strong>About me</strong> building <strong>modern, dynamic interfaces</strong> and <strong>modular, secure backends</strong>.</p>
+                                <p><strong>About me</strong> reverse engineering <strong>modern, dynamic week auth</strong> and <strong>secure, login systems</strong>.</p>
                                 <div class="Icobox">
                                         <svg class="Link" style="margin-bottom: -4px; margin-left: -1px;" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 5 50 40"><path d="M22.5 34H14q-4.15 0-7.075-2.925T4 24q0-4.15 2.925-7.075T14 14h8.5v3H14q-2.9 0-4.95 2.05Q7 21.1 7 24q0 2.9 2.05 4.95Q11.1 31 14 31h8.5Zm-6.25-8.5v-3h15.5v3ZM25.5 34v-3H34q2.9 0 4.95-2.05Q41 26.9 41 24q0-2.9-2.05-4.95Q36.9 17 34 17h-8.5v-3H34q4.15 0 7.075 2.925T44 24q0 4.15-2.925 7.075T34 34Z"/></svg>
                                     </a>
