a8a72ac76f3abc7775917ddf6a60d2c553ed2eb2
zZRapidZz <43920234+zZRapidZz@users.noreply.github.com>
2023-06-08 00:05:54 +1200

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,5 +2,9 @@ module.exports = {
     Localization: {
         Directory: "./localization"
     },
+    MongoDB: {
+        URI: "",
+        Name: "strayfade",
+        Collection: "analytics"
     }
 }
